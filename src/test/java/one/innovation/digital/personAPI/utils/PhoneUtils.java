707ade71a6dc176b8105c1419d998a118a2fb146
Ivan Henrique Santos <ivan.henrique.s.1406@gmail.com>
2021-08-13 17:52:21 -0300

    test

---
+++ src/test/java/one/innovation/digital/personAPI/utils/PhoneUtils.java
@@ -0,0 +1,27 @@
+package one.innovation.digital.personAPI.utils;
+
+import one.innovation.digital.personAPI.dto.request.PhoneDTO;
+import one.innovation.digital.personAPI.entity.Phone;
+import one.innovation.digital.personAPI.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
